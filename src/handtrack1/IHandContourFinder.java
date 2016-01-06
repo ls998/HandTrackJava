7c50c3cfaf7dca8f7d9f8eb6bfb82975c90196d6
sunny <eoflndafljn>
2016-01-06 17:29:10 -0500

    refactored method

--- src/handtrack1/IHandContourFinder.java
+++ src/handtrack1/IHandContourFinder.java
@@ -4,5 +4,5 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 
 public interface IHandContourFinder {
-	public MatOfPoint findHandContour(Mat image);
+	public void findHandContour(Mat image, MatOfPoint handContour);
 }
